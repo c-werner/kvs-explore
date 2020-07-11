08ae220d4f4cdee2e1b4d8e41f9e2ae27941acc2
c-werner <c-werner@users.noreply.github.com>
2020-07-11 10:22:49 -0700

    Add rudamentary python client lib

---
+++ python/kvsclient/client.py
@@ -0,0 +1,64 @@
+import requests
+
+TRUE = b'true'
+
+
+class KVS(object):
+    def __init__(self, host):
+        self.host = host
+
+    def get(self, key, default=None):
+        try:
+            return self[key]
+        except KeyError:
+            return default
+
+    def __getitem__(self, item):
+        resp = requests.get('http://{host}/k/{key}'.format(
+            host=self.host,
+            key=item
+        ))
+
+        if resp.status_code == 404:
+            raise KeyError
+
+        return resp.content.decode("utf-8")
+
+    def set(self, key, value):
+        requests.get('http://{host}/k/{key}?v={value}'.format(
+            host=self.host,
+            key=key,
+            value=value,
+        ))
+
+    def __setitem__(self, key, value):
+        return self.set(key, value)
+
+    def has(self, key):
+        resp = requests.get('http://{host}/h/{key}'.format(
+            host=self.host,
+            key=key,
+        ))
+        return resp.content == TRUE
+
+    def __contains__(self, item):
+        return self.has(item)
+
+    def remove(self, key):
+        resp = requests.get('http://{host}/d/{key}'.format(
+            host=self.host,
+            key=key,
+        ))
+        return resp.content == TRUE
+
+    def __delitem__(self, key):
+        return self.remove(key)
+
+    def keys(self):
+        resp = requests.get('http://{host}/list'.format(
+            host=self.host,
+        ))
+        return resp.content.decode("utf-8").split('\n')
+
+
+
